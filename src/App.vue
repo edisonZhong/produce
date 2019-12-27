21c26a91cc1b50e122bfdfde26152446b420b16b
Lex51148 <372264507@qq.com>
2019-12-27 13:56:56 +0800

    修复离职页面和选择员工页面bug

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
     <!-- <transition leave-active-class='fadeOut animated' enter-active-class='fadeIn animated'> -->
-      <!-- <keep-alive> -->
-        <router-view></router-view>
-      <!-- </keep-alive> -->
+    <!-- <keep-alive> -->
+    <router-view></router-view>
+    <!-- </keep-alive> -->
     <!-- </transition> -->
   </div>
 </template>
 
 <script>
-// import allServer from './server/allServer.js'
-export default {
-  data(){
-       return{
+  // import allServer from './server/allServer.js'
+  export default {
+    data() {
+      return {}
+    },
+    created() {
 
-       }
-   },
-   created(){
-    
+    }
   }
-}
 
 </script>
 
 <style lang="less">
   @import '~vux/src/styles/reset.less';
 
-#app{
-  height: 100%;
-}
+  #app {
+    height: 100%;
+  }
+  .h-top{
+    .seach{
+      color:#26a2ff;
+    }
+  }
+  .mint-msgbox {
+    width: 70% !important;
+    border-radius: 10px;
+
+    .mint-msgbox-message {
+      font-size: 14px;
+    }
+
+    .mint-msgbox-confirm {
+      font-size: 16px;
+    }
+  }
 </style>
